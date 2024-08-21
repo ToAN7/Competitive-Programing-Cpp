42c857e4bd44ed327d1b3af754d3398029e7abe8
ToAN7 <91736051+ToAN7@users.noreply.github.com>
2024-08-21 16:39:59 +0700

    Create folder

---
+++ based knowledge/vectorcpp.md
@@ -0,0 +1,17 @@
+---
+    "created day: ": Tue 20:08:2024
+    tags:
+        - Olympic
+    source: https://www.youtube.com/watch?v=ae4f038qHjM
+---
+
+# Initialize:
+
+vector<type> name[(size, default)] [= { a, ...}];
+
+# Vector :
+
+.push_back(value): Append the value at the end of the vector
+.pop_back(): Remove the value at the end of the vector
+.insert([location], value): Add the value before the determined location
+.resize(size_type, value): Provide a size for the vector and assign the value into it.
---
+++ openUniversity/GreedyAlgorithm1.cpp
@@ -0,0 +1,39 @@
+/*
+Mỗi ô của một bàn cờ 8×8 ghi một số nguyên dương không quá 32000. Hãy xếp 8 quân hậu lên bàn cờ sao cho không quân hậu nào ăn quân hậu nào và tổng số các ô các quân hậu đang đứng là lớn nhất. 
+Dữ liệu đầu vào trong tập tin queens.inp là bàn cờ có trọng số kích thước 8×8.
+Dữ liệu đầu ra ghi trong tập tin queens.out như sau:
+- Dòng đầu là dãy 8 số, trong đó số thứ i là vị trí cột đặt quân hậu trên dòng i.
+- Dòng thứ hai là tổng trị 8 quân hậu đứng.
+queens.inp 
+1 2 4 9 3 2 1 4 
+6 9 5 4 2 3 1 4 
+3 6 2 3 4 1 8 3 
+2 3 7 3 2 1 4 2 
+1 2 3 2 3 9 2 1 
+2 1 3 4 2 4 2 8 
+2 1 3 2 8 4 2 1 
+8 2 3 4 2 3 1 2 
+queens.out
+3 1 6 2 5 7 4 0
+66
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int arr[8][8] = {
+	{1, 2, 4, 9, 3, 2, 1, 4},
+	{6, 9, 5, 4, 2, 3, 1, 4},
+	{3, 6, 2, 3, 4, 1, 8, 3},
+	{2, 3, 7, 3, 2, 1, 4, 2},
+	{1, 2, 3, 2, 3, 9, 2, 1},
+	{2, 1, 3, 4, 2, 4, 2, 8},
+	{2, 1, 3, 2, 8, 4, 2, 1},
+	{8, 2, 3, 4, 2, 3, 1, 2}
+};
+
+int main() {
+	cout << "NeggaTon is here!!!\n";
+}
